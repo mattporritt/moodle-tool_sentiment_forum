618bd40a305eb444e1b1296c216c9af83751d098
Matt Porritt <mattp@catalyst-au.net>
2017-09-03 20:52:41 +1000

    store and retrieve sentiment fourm settings from db

--- lib.php
+++ lib.php
@@ -31,9 +31,14 @@ defined('MOODLE_INTERNAL') || die();
  * @param MoodleQuickForm $mform The actual form object (required to modify the form).
  */
 function tool_sentiment_forum_coursemodule_standard_elements($formwrapper, $mform) {
+    global $DB;
     $module = $formwrapper->get_current()->modulename;
 
     if ($module == 'forum') { // Only apply sentiment settings to forums.
+        // Get existing config
+        $forumid = $formwrapper->get_current()->id;
+        $enabled = $DB->get_field('sentiment_forum', 'enabled', array ('forumid' => $forumid));
+
         $mform->addElement('header', 'sentimentsection',
                 get_string('sentimentsection', 'tool_sentiment_forum'));
 
@@ -41,6 +46,7 @@ function tool_sentiment_forum_coursemodule_standard_elements($formwrapper, $mfor
                 get_string('sentimentenabled', 'tool_sentiment_forum'),
                 get_string('sentimentenabled_label', 'tool_sentiment_forum'),
                 false, array(0, 1));
+        $mform->setDefault('sentimentenabled', $enabled);
 
     }
 
@@ -56,15 +62,48 @@ function tool_sentiment_forum_coursemodule_standard_elements($formwrapper, $mfor
  */
 function tool_sentiment_forum_coursemodule_edit_post_actions($data, $course) {
     $module = $data->modulename;
+
     if ($module == 'forum') { // Only apply sentiment settings to forums.
         // save sentiment analysis in DB
-        error_log(print_r($data->sentimentenabled, true));
+        $record = new \stdClass();
+        $record->forumid = $data->instance;
+        $record->enabled = $data->sentimentenabled;
+        $record->timemodified = time();
+
+        sentiment_forum_upsert($record);
+
+
     }
 
     return $data;
 }
 
-function ool_sentiment_forum_pre_course_module_delete(stdClass $cm) {
+function tool_sentiment_forum_pre_course_module_delete(stdClass $cm) {
  // TODO: Handle module deletion;
 }
 
+/**
+ * Provides upsert (insert and/or update) functionality
+ * for records into the sentiment forum table.
+ *
+ * @param object $record record to update or insert.
+ * @return void
+ */
+function sentiment_forum_upsert($record) {
+    global $DB;
+    $insert = true;
+
+    // Try insert.
+    try {
+        $DB->insert_record('sentiment_forum', $record, false);
+    } catch (Exception $e) {
+        $insert = false;
+    }
+
+    // Insert failed try update.
+    if (!$insert) {
+        $id = $DB->get_field('sentiment_forum', 'id', array ('forumid' => $record->forumid));
+        $record->id = $id;
+        $DB->update_record('sentiment_forum', $record);
+    }
+}
