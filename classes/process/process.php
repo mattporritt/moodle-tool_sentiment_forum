8122e0ac35dacb16e9a98bb3bea03c3cb59c0bba
Matt Porritt <mattp@catalyst-au.net>
2018-01-27 16:12:41 +1100

    boilerplate for post analytics processing with tests. Copied from Native Learning Analytics project

---
+++ classes/process/process.php
@@ -0,0 +1,347 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Forum sentiment analyzer class.
+ *
+ * @package     tool_sentiment_forum
+ * @copyright   2017 Matt Porritt <mattp@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_sentiment_forum\process;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Analytic analyzer class.
+ *
+ * @package     tool_sentiment_forum
+ * @copyright   2017 Matt Porritt <mattp@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class process {
+
+    private $interval = 60 * 60 * 24 * 7; // One week.
+
+    /**
+     * Constructor for analyzer.
+     * Makes relevant config available.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->config = get_config('tool_sentiment_forum');
+    }
+
+
+    private function respect_dates() {
+        if ($this->config->startend == 1) {
+            $dates = true;
+        } else {
+            $dates = false;
+        }
+
+        return $dates;
+    }
+
+
+    /**
+     *
+     * @param unknown $metricname
+     * @param unknown $courseid
+     * @param number $interval
+     * @param number $now
+     * @return boolean
+     */
+    public function time_to_process($metricname, $courseid, $interval=0, $now=0) {
+        global $DB;
+        $process = false;
+
+        if ($interval == 0) {
+            $interval = $this->interval;
+        }
+
+        if ($now == 0) {
+            $now = time();
+        }
+
+        // Get last run time for metric from DB
+        $conditions = array('metricshortname' => $metricname, 'courseid' => $courseid);
+        $lastrun = $DB->get_field('tool_nla_metrics_course', 'lastrun', $conditions);
+        $nextrun = $lastrun + $interval;
+
+        // If now is greater or equal to last run + inteval return true.
+        if ($now >= $nextrun) {
+            $process = true;
+        }
+
+        return $process;
+    }
+
+
+    /**
+     *
+     * @param unknown $count
+     * @param unknown $multiplier
+     * @return number[]
+     */
+    private function calculate_index(&$count, $multiplier) {
+        $value1 = ($count + 1) * $multiplier;
+        $value2 = 0;
+
+        if (!is_int($value1)) {
+            // Need two values.
+            $value2 = round($value1, 0, PHP_ROUND_HALF_UP);
+            $value1 = round($value1, 0, PHP_ROUND_HALF_DOWN);
+        }
+
+        return array($value1, $value2);
+    }
+
+    /**
+     *
+     * @param unknown $frequency
+     * @param unknown $total
+     * @param unknown $count
+     * @param unknown $medianarray
+     * @param unknown $lowerqarray
+     * @param unknown $upperarray
+     * @return number[]|mixed[]|unknown[]
+     */
+    private function calculate_stats(&$frequency, $total, $count, $medianarray, $lowerqarray, $upperarray) {
+        $minimum = 0;
+        $maximum = 0;
+        $mean = 0;
+        $median = 0;
+        $lowerq = 0;
+        $upperq = 0;
+        $interquartilerange = 0;
+
+        // Calculate minimum value.
+        $minimum = key($frequency);
+
+        // Calculate maximum value.
+        end($frequency);
+        $maximum = key($frequency);
+        reset($frequency);
+
+        // Calculate mean value.
+        if ($count != 0 ) {
+            $mean = round(($total / $count), 3);
+        } else {
+            $mean = 0;
+        }
+        // Get median index values.
+        list($medianindex1, $medianindex2) = $medianarray;
+
+        // Get lower quartile index values.
+        list($lowerqindex1, $lowerqindex2) = $lowerqarray;
+
+        // Get upper quartile index values.
+        list($upperqindex1, $upperqindex2) = $upperarray;
+        $ncount = 0;
+        $median1 = 0;
+        $median2 = 0;
+        $lowerq1 = 0;
+        $lowerq2 = 0;
+        $upperq1 = 0;
+        $upperq2 = 0;
+
+        foreach ($frequency as $key => $value) {
+            $ncount += $value;
+
+            // Set lowerq1.
+            if (($ncount >= $lowerqindex1) && $lowerq1 == 0) {
+                $lowerq1 = $key;
+            }
+
+            // Set lowerq2.
+            if (($ncount >= $lowerqindex2) && $lowerq2 == 0) {
+                $lowerq2 = $key;
+            }
+
+            // Set median1.
+            if (($ncount >= $medianindex1) && $median1 == 0) {
+                $median1 = $key;
+            }
+
+            // Set median2.
+            if (($ncount >= $medianindex2) && $median2 == 0) {
+                $median2 = $key;
+            }
+
+            // Set upperq1.
+            if (($ncount >= $upperqindex1) && $upperq1 == 0) {
+                $upperq1 = $key;
+            }
+
+            // Set upperq2.
+            if (($ncount >= $upperqindex2) && $upperq2 == 0) {
+                $upperq2 = $key;
+            }
+
+            if (($lowerq1 != 0 && $lowerqindex2 == 0) && $lowerq == 0) {
+                $lowerq = $lowerq1;
+            } else if (($lowerq1 != 0 && $lowerq2 != 0) && $lowerq == 0) {
+                $lowerq = ($lowerq1 + $lowerq2) / 2;
+
+            }
+
+            if (($median1 != 0 && $medianindex2 == 0) && $median == 0) {
+                $median = $median1;
+            } else if (($median1 != 0 && $median2 != 0) && $median == 0) {
+                $median = ($median1 + $median2) / 2;
+            }
+
+            // Exit if we have what we need.
+            // We make the assumption that if we have the
+            // upper quartile we also have the lower quartile
+            // and the median.
+            if (($upperq1 != 0 && $upperqindex2 == 0) && $upperq == 0) {
+                $upperq = $upperq1;
+                break;
+            } else if (($upperq1 != 0 && $upperq2 != 0) && $upperq == 0) {
+                $upperq = ($upperq1 + $upperq2) / 2;
+                break;
+            }
+        }
+
+        $interquartilerange = $upperq - $lowerq;
+
+        $results = array(
+                $minimum,
+                $maximum,
+                $mean,
+                $median,
+                $lowerq,
+                $upperq,
+                $interquartilerange
+        );
+
+        return $results;
+    }
+
+    /**
+     *
+     * @param iterator $metric Metric values to calculate
+     */
+    public function get_stats($metric) {
+
+        $count = 0;
+        $total = 0;
+
+        $frequency = array();
+        foreach ($metric as $value) {
+
+            if (array_key_exists($value, $frequency)) {
+                $frequency[$value] = $frequency[$value] + 1;
+            } else {
+                $frequency[$value] = 1;
+            }
+            $count++;
+            $total += $value;
+        }
+
+        if ($frequency) {
+            ksort($frequency);  // Sort array by keys.
+
+            $medianarray = $this->calculate_index($count, 0.5);
+            $lowerqarray = $this->calculate_index($count, 0.25);
+            $upperarray  = $this->calculate_index($count, 0.75);
+
+            $results = $this->calculate_stats($frequency, $total, $count, $medianarray, $lowerqarray, $upperarray);
+        } else {
+            $results = array(0, 0, 0, 0, 0, 0, 0);
+        }
+
+        return $results;
+    }
+
+    public function save_stats($courseid, $stats, $periodstart, $periodlength) {
+        global $DB;
+        list($minimum, $maximum, $mean, $median, $lowerq, $upperq, $interquartilerange) = $stats;
+
+        $record = new \stdClass();
+        $record->courseid = $courseid;
+        $record->minimum = $minimum;
+        $record->maximum = $maximum;
+        $record->mean = $mean;
+        $record->median = $median;
+        $record->lowerquartile = $lowerq;
+        $record->upperquartile = $upperq;
+        $record->interquartile = $interquartilerange;
+        $record->modified = time();
+        $record->periodstart = $periodstart;
+        $record->periodlength = $periodlength;
+
+        $id = $DB->insert_record('tool_nla_site', $record);
+
+        return $id;
+
+    }
+
+    /**
+     *
+     * @param unknown $metricname
+     * @param number $interval
+     */
+    public function process_metric($metricname, $courseid) {
+        // If it is not time to process this metric for this course return early.
+        $processtime = $this->time_to_process($metricname, $courseid);
+        if (!$processtime) {
+            return false;
+        }
+
+        $users = $this->get_users($courseid);
+
+        if (!empty($users)) {
+            $classname = "tool_nla\\metrics\\" . $metricname;
+            $metric = new $classname($users);
+            $stats = $this->get_stats($metric);
+        } else { // No users found, so return false.
+            $stats = false;
+        }
+
+        return $stats;
+
+        // If it is time to process metric.
+            // Get metric iterator based on metric shortname.
+            // Get stats for metric.
+
+
+        // If we are calculating history for metric.
+            // Find out where we are up to in history processing.
+            // Create an adhoc task to process next lot of history (if required).
+    }
+
+    /**
+     *
+     */
+    public function process_metrics() {
+
+        foreach ($metrics as $metricname) {
+            $courses = $this->get_courses();
+            foreach ($courses as $course) {
+                $stats = $this->process_metric($metricname, $course->id);
+                // Save stats to database.
+
+            }
+        }
+
+    }
+
+}
---
+++ tests/process_test.php
@@ -0,0 +1,126 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * General metric unit tests.
+ *
+ * @package     tool_sentiment_forum
+ * @category    phpunit
+ * @copyright   2018 Matt Porritt <mattp@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+use tool_nla\analyze\analyze;
+
+class tool_sentiment_forum_process_testcase extends advanced_testcase {
+
+
+    /**
+     * Test time to process method when there are no records in DB.
+     * This occurs on initial condition when metric has never ran before.
+     */
+    public function test_time_to_process_no_record() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+
+        $course = $generator->create_course();
+
+        $analyzer = new analyze();
+        $process = $analyzer->time_to_process('test_metric', $course->id);
+
+        $this->assertEquals(true, $process);
+
+    }
+
+    /**
+     * Test time to process method when it is time to process.
+     */
+    public function test_time_to_process_time() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+
+        $course = $generator->create_course();
+        $record = new \stdClass();
+        $record->metricshortname = 'test_metric';
+        $record->courseid = $course->id;
+        $record->lastrun = 0;
+
+        $DB->insert_record('tool_nla_metrics_course', $record);
+
+        $analyzer = new analyze();
+        $process = $analyzer->time_to_process('test_metric', $course->id);
+
+        $this->assertEquals(true, $process);
+
+    }
+
+    /**
+     * Test time to process method when it is not time to process.
+     */
+    public function test_time_to_process_not_time() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $generator = $this->getDataGenerator();
+
+        $course = $generator->create_course();
+        $record = new \stdClass();
+        $record->metricshortname = 'test_metric';
+        $record->courseid = $course->id;
+        $record->lastrun = 10000;
+
+        $DB->insert_record('tool_nla_metrics_course', $record);
+
+        $analyzer = new analyze();
+        $process = $analyzer->time_to_process('test_metric', $course->id, 1000, 1000);
+
+        $this->assertEquals(false, $process);
+
+    }
+
+    /**
+     * Test method to save stats to database.
+     */
+    public function test_save_stats() {
+        global $DB;
+        $this->resetAfterTest(true);
+
+        $courseid = 1;
+        $stats = array(2, 3, 4, 5, 6, 7, 8);
+        $periodstart = 100;
+        $periodlength = 604800;
+
+        $analyzer = new analyze();
+        $id = $analyzer->save_stats($courseid, $stats, $periodstart, $periodlength);
+
+        $record = $DB->get_record('tool_nla_site', array('id' => $id));
+
+        $this->assertEquals($courseid, $record->courseid);
+        $this->assertEquals(2, $record->minimum);
+        $this->assertEquals(3, $record->maximum);
+        $this->assertEquals(4, $record->mean);
+        $this->assertEquals(5, $record->median);
+        $this->assertEquals(6, $record->lowerquartile);
+        $this->assertEquals(7, $record->upperquartile);
+        $this->assertEquals(8, $record->interquartile);
+        $this->assertEquals($periodstart, $record->periodstart);
+        $this->assertEquals($periodlength, $record->periodlength);
+
+    }
+}
